09fc899bbee11b04f3d960191e78a6cd802b7563
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-08-19 14:27:58 +0900

    tooltip ui 작업

--- src/stories/ui/Tooltip.tsx
+++ src/stories/ui/Tooltip.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import * as RadixTooltip from '@radix-ui/react-tooltip';
+import { PlusIcon } from '@radix-ui/react-icons';
+import './Tooltip.css';
 
 const Tooltip = () => {
     return (
-        <h1>tooltip</h1>
+        <RadixTooltip.Provider>
+            <RadixTooltip.Root>
+                <RadixTooltip.Trigger asChild>
+                    <button className="IconButton">
+                        <PlusIcon />
+                    </button>
+                </RadixTooltip.Trigger>
+                <RadixTooltip.Portal>
+                    <RadixTooltip.Content className="TooltipContent" sideOffset={5}>
+                        Add to library
+                        <RadixTooltip.Arrow className="TooltipArrow" />
+                    </RadixTooltip.Content>
+                </RadixTooltip.Portal>
+            </RadixTooltip.Root>
+        </RadixTooltip.Provider>
     )
 }
 
