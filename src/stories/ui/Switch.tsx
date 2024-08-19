1544d55904d76e2b944ab17ec8b35804e51ed6bf
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-08-19 14:41:57 +0900

    switch ui 작업

--- src/stories/ui/Switch.tsx
+++ src/stories/ui/Switch.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import * as RadixSwitch from '@radix-ui/react-switch';
+import './Switch.css';
 
-const Switch = () => {
-    return (
-        <div>
-            switch
+const SwitchDemo = () => (
+    <form>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <label className="Label" htmlFor="airplane-mode" style={{ paddingRight: 15 }}>
+                Airplane mode
+            </label>
+            <RadixSwitch.Root className="SwitchRoot" id="airplane-mode">
+                <RadixSwitch.Thumb className="SwitchThumb" />
+            </RadixSwitch.Root>
         </div>
-    )
-}
+    </form>
+);
 
-export default Switch
-
+export default SwitchDemo;
